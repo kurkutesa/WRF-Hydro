c2ecf25d20cf5955dca1b6f816d0799634cbb5d6
micha-silver <micha@arava.co.il>
2013-10-27 16:51:20 +0200

    Added checking of netcdf files to change_nc.py

--- change_nc.py
+++ change_nc.py
@@ -11,82 +11,148 @@
   Command line options:
   -i|--input      : input netcdf filename
   -o|--output     : output netcdf filename
-  -c|--change-var : variable name to be changed
-  -n|--new-value  : new value to be inserted into the variable
+  -c|--change-tbl : table (csv format) of mask id values and new values to be applied
   -m|--mask-var   : a variable in the netcdf used as a mask. 
-  -v|--mask-value : the value of the --mask-var to be used as a mask
 
-  Only those grid cells for which the variable --mask-var contains the value --mask-val 
+  Only those grid cells for which the variable --mask-var contains the value n the change-tbl 
   are used to apply the change. 
-  THose cells covered by the mask will have their variable --change-var 
-  altered to the value of --new-value
+  THose cells covered by the mask will have their variable --mask-var 
+  altered to the value obtained from the change-tbl
 
 """
 
-import os, getopt, sys
+import os, getopt, sys, csv
+import shutil
 import scipy.io.netcdf as nc
 
-def print_syntax():
+
+def print_syntax(m):
+  print m
   print ('Syntax: \n\tchange_nc.py -h|--help (this message)')
   print ('\tchange_nc.py -i|--input <input netcdf> -o|--output <output netcdf>')
-  print ('\t\t-c|--change-var <variable to be changed> -n|--new-value <new value to be inserted>')
-  print ('\t\t-m|--mask-var <variable used as mask> -v|--mask-value <value used as mask>')
+  print ('\t\t-c|--change-tbl <table of ids and new values> -m|--mask-var <variable used as mask>')
+
 
 def get_options(argv):
   try:
-    opts, args = getopt.getopt(argv, 'hi:o:c:n:m:v:', '[help, input=,output=,change-var=,new-value=,mask-var=,mask-value=]')
+    opts, args = getopt.getopt(argv, 'hi:o:c:m:', ['help','input=','output=','change-tbl=','mask-var='])
   except getopt.GetoptError:
-    print_syntax()
+    print_syntax("Options error")
     return False
 
 # Initialize option names to empty strings
-  in_nc =     ''
-  out_nc =    ''
-  change_var =''
-  new_val =   ''
-  mask_var =  ''
-  mask_value =''
-
+  in_nc       = ''
+  out_nc      = ''
+  change_tbl  = ''
+  mask_var    = ''
+  syntax_msg  = ''
   for opt, arg in opts:
     if opt in ('-h','-help'):
-      print_syntax()
+      print_syntax(syntax_msg)
       return False
     elif opt in ("-i", "--input"):
       in_nc = arg
     elif opt in ("-o", "--output"):
       out_nc = arg
-    elif opt in ("-c", "--change-var"):
-      change_var = arg
-    elif opt in ("-n", "--new-value"):
-      new_val = arg
+    elif opt in ("-c", "--change-tbl"):
+      change_tbl = arg
     elif opt in ("-m", "--mask-var"):
       mask_var = arg
-    elif opt in ("-v", "--mask-value"):
-      mask_value = arg
 
-  options = {'in_nc':in_nc,'out_nc':out_nc,'change_var':change_var,'new_val':new_val, 'mask_var':mask_var,'mask_value':mask_value}
+  options = {'in_nc':in_nc,'out_nc':out_nc,'change_tbl':change_tbl, 'mask_var':mask_var}
 
   # Make sure all options passed on command line
   have_options = True
   for k, v in options.iteritems():
     if len(v) == 0:
       have_options = False
+    if k == 'in_nc' and len(options[k])==0:
+      syntax_msg += "No input nc file\n"
+      have_options = False
+    if k == 'out_nc' and len(options[k])==0:
+      syntax_msg += "No output nc file\n"
+      have_options = False
+    if k == 'change_tbl' and len(options[k])==0:
+      syntax_msg += "No mask table file\n"
+      have_options = False
+    if k == 'mask_var' and len(options[k])==0:
+      syntax_msg += "No mask variable\n"
+      have_options = False
   
   if (have_options == True):
     return options
   else:
-    print_syntax()
+    print_syntax(syntax_msg)
     return False
+
+
+
+def read_mask_values(change_tbl):
+  """
+  Reads the CSV file change_tbl, skipping comment lines (beginning with #)
+  collects mask values into a list, indexed by the mask ids
+  """
+  try:
+    f = open(change_tbl, 'r')
+    mask_csv = csv.reader(f, delimiter=',')
+    mask_dict={}
+    for ln in mask_csv:
+      if not ln[0].startswith('#'):
+        mask_dict[ln[0]]=ln[1]
+  except IOError, e:
+    print ("Changes table file not available"+str(e))
+    return False
+
+  # test
+  for i,v in mask_dict.iteritems():
+    print "Mask id: "+ i + "\t New value: "+mask_dict[i]
+  return mask_dict
+
+
+
+def check_nc_files(in_file, out_file, var):
+  """
+  Verify the the input nc file is available.
+  If the output nc is not available, create it
+  check that the VARIABLE exists in input file
+  """
+  try:
+    os.path.isfile(in_file)
+  except:
+    print "Input file not available"
+    return False
+
+  if not os.path.isfile(out_file):
+    shutil.copy(in_file, out_file)
+
+  in_nc = nc.Dataset(in_file, 'r')
+  out_nc = nc.Dataset(out_file, 'w')
+  in_var = in_nc.variables(var)
+  out_var = out_nc.variables(var)
+  if in_var.dimensions == out_var.dimensions and in_var.shape == out_var.shape:
+    return True
+
+  return False
 
 
 def main(argv):
   options = get_options(argv)
   if (options == False):
-    print 'Enter all options'
+    print '\nEnter all options\n'
   else:
-    print 'Options entered:'
-    for k in options:
-      print '\t'+k+' = '+options[k]
+    #print 'Options entered:'
+    #for k in options:
+    #  print '\t'+k+' = '+options[k]
+    print "Altering variable: "+ options['mask_var'] + "\t Changing output netcdf: "+ options['out_nc']
+    mask_dict = read_mask_values(options['change_tbl'])
+
+    # check input and output nc files
+    if not (check_nc_files(options['in_nc'],options['out_nc'],options['mask_var'])):
+      print "Netcdf files not available"
+      sys.exit(0)
+    else:
+    # Files check out. Good to go
+      print "Proceeding with variable replacement"
 
 """
 # Open wrfout for reading, new netcdf for writing
