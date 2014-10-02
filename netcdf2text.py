164205f685853da9be2c76f75ba04b1a223bbaa0
Micha Silver <micha@arava.co.il>
2014-10-02 12:05:42 +0300

    The netcdf2txt python script (v 0.1) finished

--- netcdf2text.py
+++ netcdf2text.py
@@ -7,48 +7,54 @@ Author:		Micha Silver
 Date:		1/10/2014
 """
 import netCDF4
-import os,csv,argparse
+import numpy as np
+import os,csv,argparse,glob
 
-def parse_netcdf(ncname,ncdir,outdir):
+def parse_netcdf(ncdir,outdir):
+	wrfout_pattern = "wrfout_d03_"
+	wrfoutlist = glob.glob(os.path.join(ncdir,wrfout_pattern+"*"))
+	for ncname in wrfoutlist:
+		print ("Working on: %s" % (ncname,))
+			
+		ncpath = os.path.join(ncdir, ncname)
+		ncfile = netCDF4.Dataset(ncpath, 'r')
+		lat = ncfile.variables['XLAT'][:]
+		lon = ncfile.variables['XLONG'][:]
+		rain = ncfile.variables['RAINNC'][:]
+		# Each wrfout netCDF variable has 3 dimensions:
+		print ("Dimentions of netCDF variables: %s" % (rain.shape,))
+		# Use the 2nd and third to size the outdata array
+		ncols, nrows = len(rain[0]), len(rain[0][0])
+		print("Lengths: LAT- %s, LON- %s" % (ncols, nrows))
+		outdata = np.empty([3,ncols*nrows])
+		i = 0
+		for y in range(nrows):
+		    for x in range(ncols):
+			outdata[0][i] = lat[0][x][y]
+			outdata[1][i] = lon[0][x][y]
+			outdata[2][i] = rain[0][x][y]
+			i = i+1
+		
+		print ("Outdata contains: %s rows" % (i,))
+		ncfile.close()
+		
+		datestr = os.path.basename(ncname)[11:24]
+		outname = "precip_"+datestr+".txt"	
+		print ("Saving to output file: %s\n" % (outname,))	
+		outpath = os.path.join(outdir, outname)
+		csvfile = open(outpath, "w")
+		writer = csv.writer(csvfile)
+		for r in range(ncols*nrows):
+			writer.writerows( [(outdata[0][r], outdata[1][r], outdata[2][r])] )
 
-	ncpath = os.path.join(ncdir,ncname)	
-	ncfile = netCDF4.Dataset(ncpath, 'r')
-	lat = ncfile.variables['XLAT'][:]
-	lon = ncfile.variables['XLONG'][:]
-	rain = ncfile.variables['RAINNC'][:]
-
-	nlat, nlon = len(lat), len(lon)
-	datestr = ncname[11:24]
-	outname = "precip_"+datestr+".txt"
-	outdata = []
-
-	for y in range(nlat):
-	    for x in range(nlon):
-		outdata.append((x,y,rain[x,y]))
-
-	ncfile.close()
-	
-	outpath = os.path.join(outdir, outname)
-	csvfile = open(outpath, "w")
-	writer = csv.writer(csvfile)
-	for row in outdata:
-		writer.writerows("%s,%s,%s" % (row[0], row[1], row[2]) )
-
-	csvfile.close()
+		csvfile.close()
 
 
 # Main work starts here
 parser = argparse.ArgumentParser("Get command line arguments")
-parser.add_argument("-n", "--ncfile", required=True, help="wrfout netCDF file")
-parser.add_argument("-i", "--indir", default=".", help="Directory of wrfout netCDF files") 
+parser.add_argument("-i", "--wrfdir", default=".", required=True, help="Directory of wrfout netCDF files") 
 parser.add_argument("-o", "--outdir", default=".", help="Directory to store output csv")
 
 args = parser.parse_args()
-parse_netcdf(args.ncfile, args.indir, args.outdir)
+parse_netcdf(args.wrfdir, args.outdir)
 
-
-
-# Using ImageMagick convert:
-# http://blog.room208.org/post/48793543478
-# $ convert -fuzz 1% -delay 1x8 `seq -f %03g.png 10 3 72` \
-#                  -coalesce -layers OptimizeTransparency animation.gif
