132744ea399da67b9a6ccc8e44c9c5deb7bfdb77
Micha Silver <micha@arava.co.il>
2014-10-02 16:04:27 +0300

    Cosmetic changes to netcdf2text

--- netcdf2text.py
+++ netcdf2text.py
@@ -10,7 +10,7 @@ import netCDF4
 import numpy as np
 import os,csv,argparse,glob
 
-def parse_netcdf(ncdir,outdir):
+def netcdf_to_text(ncdir,outdir):
 	wrfout_pattern = "wrfout_d03_"
 	wrfoutlist = glob.glob(os.path.join(ncdir,wrfout_pattern+"*"))
 	for ncname in wrfoutlist:
@@ -22,16 +22,18 @@ def parse_netcdf(ncdir,outdir):
 		lon = ncfile.variables['XLONG'][:]
 		rain = ncfile.variables['RAINNC'][:]
 		# Each wrfout netCDF variable has 3 dimensions:
-		print ("Dimentions of netCDF variables: %s" % (rain.shape,))
+		#print ("Dimentions of netCDF variables: %s" % (rain.shape,))
 		# Use the 2nd and third to size the outdata array
 		ncols, nrows = len(rain[0]), len(rain[0][0])
-		print("Lengths: LAT- %s, LON- %s" % (ncols, nrows))
+		#print("Lengths: LAT-%s, LON-%s" % (ncols, nrows))
+		# Output data array will have 3 columns(lat, long, precip) 
+		# and enough rows for all the data cells in original netCDF
 		outdata = np.empty([3,ncols*nrows])
 		i = 0
 		for y in range(nrows):
 		    for x in range(ncols):
-			outdata[0][i] = lat[0][x][y]
-			outdata[1][i] = lon[0][x][y]
+			outdata[0][i] = lon[0][x][y]
+			outdata[1][i] = lat[0][x][y]
 			outdata[2][i] = rain[0][x][y]
 			i = i+1
 		
@@ -39,7 +41,7 @@ def parse_netcdf(ncdir,outdir):
 		ncfile.close()
 		
 		datestr = os.path.basename(ncname)[11:24]
-		outname = "precip_"+datestr+".txt"	
+		outname = "precip_csv_"+datestr+".txt"	
 		print ("Saving to output file: %s\n" % (outname,))	
 		outpath = os.path.join(outdir, outname)
 		csvfile = open(outpath, "w")
@@ -54,7 +56,8 @@ def parse_netcdf(ncdir,outdir):
 parser = argparse.ArgumentParser("Get command line arguments")
 parser.add_argument("-i", "--wrfdir", default=".", required=True, help="Directory of wrfout netCDF files") 
 parser.add_argument("-o", "--outdir", default=".", help="Directory to store output csv")
-
+# Get arguments
 args = parser.parse_args()
-parse_netcdf(args.wrfdir, args.outdir)
+# Run the function
+netcdf_to_text(args.wrfdir, args.outdir)
 
